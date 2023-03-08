33653abc36ebcb0a76f71350dd5f41e643356280
권태준 <44458173+Gwontaejun@users.noreply.github.com>
2023-03-08 18:06:58 +0900

    2023-03-08
    :Detailed Notes:
    - Google Map 추가
    - 민감한 정보 env파일에 저장 (api key)

--- src/pages/Map/MapPage.tsx
+++ src/pages/Map/MapPage.tsx
@@ -1,4 +1,6 @@
 import React, { useRef, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+import MapStylesheet from './MapStylesheet.json';
 import 'assets/styles/MapPage.scss';
 
 const MapPage = () => {
@@ -13,7 +15,16 @@ const MapPage = () => {
 				<div className="stamp stamp3" />
 			</div>
 
-			<div className="map-content">DDDD</div>
+			<div className="map-content">
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+					options={{
+						styles: MapStylesheet,
+					}}
+					defaultCenter={{ lat: 37.6009055, lng: 126.9485623 }}
+					defaultZoom={1}
+				/>
+			</div>
 		</div>
 	);
 };
